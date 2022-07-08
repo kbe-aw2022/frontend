6a27af0b5ece1f9744b4cf24bed91641b67f803d
wklein1 <Witali@>
2022-07-08 16:19:02 +0200

    KA-12 add products context

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -3,14 +3,17 @@ import Header from "../../components/header/Header";
 import SideBar from "../../components/sideBar/SideBar";
 import GridView from "../../components/gridView/GridView";
 import ViewContextProvider from "../../store/view-context";
+import ProductsContextProvider from "../../store/products-context";
 
 const MainPage:React.FC = () => {
     return(
         <div className="main">
             <ViewContextProvider>
-                <Header></Header>
                 <SideBar></SideBar>
-                <GridView></GridView>
+                <ProductsContextProvider>
+                    <Header></Header>
+                    <GridView></GridView>
+                </ProductsContextProvider>
             </ViewContextProvider>
         </div>
     );
