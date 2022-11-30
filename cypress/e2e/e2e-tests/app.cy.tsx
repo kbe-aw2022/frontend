4fafa564678702051dafafa3e9bec5e826c0ec52
wklein1 <Witali@>
2022-11-30 20:55:25 +0100

    KA-42 update cypress tests

--- cypress/e2e/e2e-tests/app.cy.tsx
+++ cypress/e2e/e2e-tests/app.cy.tsx
@@ -1,23 +1,19 @@
 /// <reference types="cypress" />
 
-describe("basic-tests", ()=>{
+describe("auth-tests", ()=>{
     
     beforeEach(()=>{
         cy.visit("http://localhost:3000/")
 
     })
-    it("contains company name",()=>{
-        cy.contains("Computer")
-    })
     
-    it("contains login button",()=>{
+    it("user logs in",()=>{
         cy.get("#log-in-button").click()
-        // cy.get("#Username/Email-input").type("test")
-        cy.get('input[name="Username/E-mail:-input"]').type('test')
-        cy.get('input[name="Password:-input"]').type('testtest2')
-        cy.get(".Form_submit-button__66Ysv").click()
-
-
+        .get('input[name="Username/E-mail:-input"]').type('JohnDoe')
+        .get('input[name="Password:-input"]').type('dummypassword1')
+        .get(".Form_submit-button__66Ysv").click()
+        cy.contains("JohnDoe")
     })
+
 
 })
